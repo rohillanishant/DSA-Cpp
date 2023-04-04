c6e2c483aa192444f1172888ab32df6e69de6c3a
Nishant <rohillanishant19@gmail.com>
2023-04-04 21:17:25 +0530

    ArrayPrint using recursion

---
+++ Recursion/ArrayPrint.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void print(int arr[],int n){
+    if(n==0){
+        return;
+    }
+    cout<<arr[0]<<" ";
+    print(arr+1,n-1);
+}
+int main() {
+    int arr[]={4,2,4,54,8};
+    print(arr,5);
+    return 0;
+}
