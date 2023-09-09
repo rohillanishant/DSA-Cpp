4d9e1f8075a86497231e5daabfe65dba072473be
Nishant <rohillanishant19@gmail.com>
2023-09-09 20:48:57 +0530

    InPlace MergeSort

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -1,17 +1,13 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void sort(int *arr,int start,int end){
-    if(start>=end){
-        return;
-    }
+//T.C= O(nlogn) S.C= O(1)
+void merge(int arr[],int start,int end){
     int mid=start/2+end/2;
     int  len1=mid-start+1;
     int *left=new int[len1];
     int len2=end-mid;
     int *right=new int[len2];
-    sort(arr,start,mid);
-    sort(arr,mid+1,end);
     int k=start;
     for(int i=0;i<len1;i++){
         left[i]=arr[k++];
@@ -35,9 +31,18 @@ void sort(int *arr,int start,int end){
         arr[index++]=right[j++];
     }
 }
+void mergeSort(int *arr,int start,int end){
+    if(start>=end){
+        return;
+    }
+    int mid=start/2+end/2;
+    mergeSort(arr,start,mid);
+    mergeSort(arr,mid+1,end);
+    merge(arr,start,end);
+}
 int main() {
     int arr[]={83,3,7,27,59,1,46};
-    sort(arr,0,8);
+    mergeSort(arr,0,8);
     for(int i=0;i<7;i++){
         cout<<arr[i]<<' ';
     }
